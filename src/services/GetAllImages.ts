ca4e17689984d50437d87d6c3799d1862a8a9ac1
Bilihó Bence <biliho.bence@olm.hu>
2025-03-10 10:12:22 +0100

    Refactor file reading and gallery image retrieval for improved path handling; update Navbar styles for better responsiveness

--- src/services/GetAllImages.ts
+++ src/services/GetAllImages.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import {readdirSync} from "node:fs";
+import path from "node:path";
 
 const DIR_PATH = 'public/gallery';
 
-export const getAllFiles = async () => readdirSync(DIR_PATH);
+export const getAllFiles = async () => readdirSync(path.join(process.cwd(), DIR_PATH));
